d0d3bb22e83a8eb031588004ce324865c752c894
Anneke McGrady <annekemcgrady@gmail.com>
2019-04-13 10:52:42 -0600

    Format HTML and CSS to match comp layout

---
+++ main.js
@@ -0,0 +1,6 @@
+var searchInput = document.querySelector('.search-input');
+var taskTitleInput = document.querySelector('.task-title');
+var taskItemInput = docuement.querySelector('.task-item');
+var cardField = document.querySelector('.card-field');
+
+
