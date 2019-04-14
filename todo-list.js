eb95393909ac0f264115869ef55f93e1f1c48062
Anneke McGrady <annekemcgrady@gmail.com>
2019-04-14 15:12:49 -0600

    Make cards persist on page reload

--- todo-list.js
+++ todo-list.js
@@ -1,18 +1,25 @@
 class Task {
-  constructor(title, items){
+  constructor(title, items, id, urgent){
     this.title = title;
     this.items = items;
     this.id = Date.now();
     this.urgent = false;
   }
 
-saveToStorage(){
+  saveToStorage() { 
+    var stringifiedToDoArray = JSON.stringify(allTaskCards);
+    localStorage.setItem('allTaskCards', stringifiedToDoArray);
+    };
 
-}
-
-deleteFromStorage(){
-
-}
+    //copied from IdeaBox need to update vars to this project
+// deleteFromStorage(targetId) {
+//   var parsedItems = JSON.parse(localStorage.getItem('ideaArray'));
+//   var itemIndex = parsedItems.findIndex(function(idea) {
+//     return idea.id === targetId;
+//   })
+//   parsedItems.splice(itemIndex, 1);
+//   localStorage.setItem('ideaArray', JSON.stringify(parsedItems));
+//   };
 
 // updateToDo (should update the todo’s title and urgency)
 // updateTask (should update a task’s content and if it has been completed)
@@ -25,3 +32,6 @@ class TaskItems {
   this.id = Date.now();
   }
 };
+
+
+
